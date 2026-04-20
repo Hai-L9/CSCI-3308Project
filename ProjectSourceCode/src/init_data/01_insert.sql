59dc314b40714562c899c4d2f851d9416711e9ea
Winston <65579453+TWinston-66@users.noreply.github.com>
2026-04-20 17:53:22 -0600

    fix: spelling

--- ProjectSourceCode/src/init_data/01_insert.sql
+++ ProjectSourceCode/src/init_data/01_insert.sql
@@ -3,7 +3,7 @@ INSERT INTO users (username, email, password_hash, role)
 VALUES ('test_worker', 'test_worker@meow.com', '$2b$10$12qYfJ6m1nvwaOC2m6glb.t7FNKTjCUPl5lbHO9MtGIybhoFz1tXy', 'worker')
 ON CONFLICT DO NOTHING;
 
--- Default admin user: meowM / moewM@meow.com / password: meow
+-- Default admin user: meow / moew@meow.com / password: meow
 INSERT INTO users (username, email, password_hash, role)
-VALUES ('meow', 'moew@meow.com', '$2b$10$12qYfJ6m1nvwaOC2m6glb.t7FNKTjCUPl5lbHO9MtGIybhoFz1tXy', 'admin')
+VALUES ('meow', 'meow@meow.com', '$2b$10$12qYfJ6m1nvwaOC2m6glb.t7FNKTjCUPl5lbHO9MtGIybhoFz1tXy', 'admin')
 ON CONFLICT DO NOTHING;
